fc89a159e81159d50dab2041ead59965b7ea09d6
trueenglishcoder <79080325+trueenglishcoder@users.noreply.github.com>
2023-04-13 11:05:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.weightstats import ttest_ind
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 346373029 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _, pval, _ = ttest_ind(x, y, alternative = 'larger')
+    return pval < 0.09
